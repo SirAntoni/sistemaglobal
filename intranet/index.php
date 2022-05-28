b0a905f74c046adc0fa88dda78cc4d55896c9465
Antony Culqui <a.culqui02@gmail.com>
2022-05-28 00:07:32 -0500

    Se agrego estructura intranet

---
+++ intranet/index.php
@@ -0,0 +1,460 @@
+<!doctype html>
+<html class="fixed sidebar-light sidebar-left-sm header-dark">
+	<head>
+
+		<!-- Basic -->
+		<meta charset="UTF-8">
+
+		<title>Area de revendedores | Sistema Global.</title>
+		<meta name="keywords" content="Sistema Global" />
+		<meta name="description" content="Sistema Global">
+		<meta name="author" content="Antony Culqui">
+
+		<!-- Favicons -->
+		<link href="https://i.imgur.com/ZjxPBUa.png" rel="icon">
+  <link href="https://i.imgur.com/ZjxPBUa.png" rel="apple-touch-icon">
+
+		<!-- Mobile Metas -->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+
+		<!-- Web Fonts  -->
+		<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800|Shadows+Into+Light" rel="stylesheet" type="text/css">
+
+		<!-- Vendor CSS -->
+		<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.css" />
+		<link rel="stylesheet" href="vendor/animate/animate.compat.css">
+		<link rel="stylesheet" href="vendor/font-awesome/css/all.min.css" />
+		<link rel="stylesheet" href="vendor/boxicons/css/boxicons.min.css" />
+		<link rel="stylesheet" href="vendor/magnific-popup/magnific-popup.css" />
+		<link rel="stylesheet" href="vendor/bootstrap-datepicker/css/bootstrap-datepicker3.css" />
+		<link rel="stylesheet" href="vendor/jquery-ui/jquery-ui.css" />
+		<link rel="stylesheet" href="vendor/jquery-ui/jquery-ui.theme.css" />
+		<link rel="stylesheet" href="vendor/bootstrap-multiselect/css/bootstrap-multiselect.css" />
+		<link rel="stylesheet" href="vendor/morris/morris.css" />
+
+		<!-- Theme CSS -->
+		<link rel="stylesheet" href="css/theme.css" />
+
+		<!-- Skin CSS -->
+		<link rel="stylesheet" href="css/skins/default.css" />
+
+		<!-- Theme Custom CSS -->
+		<link rel="stylesheet" href="css/custom.css">
+
+		<!-- Head Libs -->
+		<script src="vendor/modernizr/modernizr.js"></script>
+
+	</head>
+	<body>
+		<section class="body">
+
+			<!-- start: header -->
+			<header class="header">
+				<div class="logo-container">
+					<a href="#" class="logo">
+						<img src="img/sistema-global-logo.png" width="180" height="44" alt="Porto Admin" />
+					</a>
+
+					<div class="d-md-none toggle-sidebar-left" data-toggle-class="sidebar-left-opened" data-target="html" data-fire-event="sidebar-left-opened">
+						<i class="fas fa-bars" aria-label="Toggle sidebar"></i>
+					</div>
+
+				</div>
+
+				<!-- start: search & user box -->
+				<div class="header-right">
+
+					<span class="separator"></span>
+
+					<div id="userbox" class="userbox">
+						<a href="#" data-bs-toggle="dropdown">
+							<figure class="profile-picture">
+								<img src="img/!logged-user.jpg" alt="Joseph Doe" class="rounded-circle" data-lock-picture="img/!logged-user.jpg" />
+							</figure>
+							<div class="profile-info" data-lock-name="John Doe" data-lock-email="johndoe@okler.com">
+								<span class="name">Antony Alfredo Culqui Carranza</span>
+								<span class="role">Administrator</span>
+							</div>
+
+							<i class="fa custom-caret"></i>
+						</a>
+
+						<div class="dropdown-menu">
+							<ul class="list-unstyled mb-2">
+								<li class="divider"></li>
+								<li>
+									<a role="menuitem" tabindex="-1" href="#"><i class="bx bx-user-circle"></i> Mi perfil</a>
+								</li>
+								<li>
+									<a role="menuitem" tabindex="-1" href="#"><i class="bx bx-power-off"></i> Cerrar sesión</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<!-- end: search & user box -->
+			</header>
+			<!-- end: header -->
+
+			<div class="inner-wrapper">
+				<!-- start: sidebar -->
+				<aside id="sidebar-left" class="sidebar-left">
+
+				    <div class="sidebar-header">
+				        <div class="sidebar-title">
+				            Navegación
+				        </div>
+				        <div class="sidebar-toggle d-none d-md-block" data-toggle-class="sidebar-left-collapsed" data-target="html" data-fire-event="sidebar-left-toggle">
+				            <i class="fas fa-bars" aria-label="Toggle sidebar"></i>
+				        </div>
+				    </div>
+
+				    <div class="nano">
+				        <div class="nano-content">
+				            <nav id="menu" class="nav-main" role="navigation">
+
+				                <ul class="nav nav-main">
+				                    <li>
+				                        <a class="nav-link" href="#">
+				                            <i class="bx bx-home-alt" aria-hidden="true"></i>
+				                            <span>Area de revendedor</span>
+				                        </a>                        
+				                    </li>
+									<li>
+				                        <a class="nav-link" href="#">
+				                            <i class="bx bx-cog" aria-hidden="true"></i>
+				                            <span>Configuración</span>
+				                        </a>                        
+				                    </li>
+									<li>
+				                        <a class="nav-link" href="#">
+				                            <i class="bx bx-like" aria-hidden="true"></i>
+				                            <span>Sistema</span>
+				                        </a>                        
+				                    </li>
+									<li>
+				                        <a class="nav-link" href="#">
+				                            <i class="bx bx-bar-chart-alt-2" aria-hidden="true"></i>
+				                            <span>Trafico</span>
+				                        </a>                        
+				                    </li>
+									<li>
+				                        <a class="nav-link" href="#">
+				                            <i class="bx bx-category" aria-hidden="true"></i>
+				                            <span>Complementos</span>
+				                        </a>                        
+				                    </li>
+									<li>
+				                        <a class="nav-link" href="#">
+				                            <i class="bx bx-wrench" aria-hidden="true"></i>
+				                            <span>Herramientas</span>
+				                        </a>                        
+				                    </li>
+									<li>
+				                        <a class="nav-link" href="#">
+				                            <i class="bx bxs-graduation" aria-hidden="true"></i>
+				                            <span>Escuela</span>
+				                        </a>                        
+				                    </li>
+									<li>
+				                        <a class="nav-link" href="#">
+				                            <i class="bx bx-support" aria-hidden="true"></i>
+				                            <span>Soporte</span>
+				                        </a>                        
+				                    </li>
+				                    <!--li class="nav-parent">
+				                        <a class="nav-link" href="#">
+				                            <i class="bx bx-cart-alt" aria-hidden="true"></i>
+				                            <span>eCommerce</span>
+				                        </a>
+				                        <ul class="nav nav-children">
+				                            <li>
+				                                <a class="nav-link" href="ecommerce-dashboard.html">
+				                                    Dashboard
+				                                </a>
+				                            </li>
+				                            <li>
+				                                <a class="nav-link" href="ecommerce-products-list.html">
+				                                    Products List
+				                                </a>
+				                            </li>
+				                            <li>
+				                                <a class="nav-link" href="ecommerce-products-form.html">
+				                                    Products Form
+				                                </a>
+				                            </li>
+				                            <li>
+				                                <a class="nav-link" href="ecommerce-category-list.html">
+				                                    Categories List
+				                                </a>
+				                            </li>
+				                            <li>
+				                                <a class="nav-link" href="ecommerce-category-form.html">
+				                                    Category Form
+				                                </a>
+				                            </li>
+				                            <li>
+				                                <a class="nav-link" href="ecommerce-coupons-list.html">
+				                                    Coupons List
+				                                </a>
+				                            </li>
+				                            <li>
+				                                <a class="nav-link" href="ecommerce-coupons-form.html">
+				                                    Coupons Form
+				                                </a>
+				                            </li>
+				                            <li>
+				                                <a class="nav-link" href="ecommerce-orders-list.html">
+				                                    Orders List
+				                                </a>
+				                            </li>
+				                            <li>
+				                                <a class="nav-link" href="ecommerce-orders-detail.html">
+				                                    Orders Detail
+				                                </a>
+				                            </li>
+				                            <li>
+				                                <a class="nav-link" href="ecommerce-customers-list.html">
+				                                    Customers List
+				                                </a>
+				                            </li>
+				                            <li>
+				                                <a class="nav-link" href="ecommerce-customers-form.html">
+				                                    Customers Form
+				                                </a>
+				                            </li>
+				                        </ul>
+				                    </li-->
+				                </ul>
+				            </nav>
+
+				            <hr class="separator" />
+				        </div>
+
+				        <script>
+				            // Maintain Scroll Position
+				            if (typeof localStorage !== 'undefined') {
+				                if (localStorage.getItem('sidebar-left-position') !== null) {
+				                    var initialPosition = localStorage.getItem('sidebar-left-position'),
+				                        sidebarLeft = document.querySelector('#sidebar-left .nano-content');
+
+				                    sidebarLeft.scrollTop = initialPosition;
+				                }
+				            }
+				        </script>
+
+				    </div>
+
+				</aside>
+				<!-- end: sidebar -->
+
+				<section role="main" class="content-body">
+					<header class="page-header">
+						<h2>Dashboard</h2>
+
+						<div class="right-wrapper text-end">
+							<ol class="breadcrumbs">
+								<li>
+									<a href="index.php">
+										<i class="bx bx-home-alt"></i>
+									</a>
+								</li>
+
+								<!--li><span>Layouts</span></li-->
+
+
+							</ol>
+
+							<a class="sidebar-right-toggle" data-open="sidebar-right"><i class="fas fa-chevron-left"></i></a>
+						</div>
+					</header>
+
+					<!-- start: page -->
+					<div class="row">
+						<div class="col-lg-12 mb-3">
+							<section class="card">
+								<div class="card-body">
+									<div class="row">
+										<div class="col-xl-8">
+											<div class="chart-data-selector" id="salesSelectorWrapper">
+												<h2>
+													Invitados:
+													<strong>
+														<select class="form-control" id="salesSelector">
+															<option value="Porto Admin" selected>Miembros 1</option>
+															<option value="Porto Drupal" >Miembros 2</option>
+															<option value="Porto Wordpress" >Miembros 3</option>
+														</select>
+													</strong>
+												</h2>
+
+												<div id="salesSelectorItems" class="chart-data-selector-items mt-3">
+													<!-- Flot: Sales Porto Admin -->
+													<div class="chart chart-sm" data-sales-rel="Porto Admin" id="flotDashSales1" class="chart-active" style="height: 203px;"></div>
+													<script>
+
+														var flotDashSales1Data = [{
+														    data: [
+														        ["Jan", 140],
+														        ["Feb", 240],
+														        ["Mar", 190],
+														        ["Apr", 140],
+														        ["May", 180],
+														        ["Jun", 320],
+														        ["Jul", 270],
+														        ["Aug", 180]
+														    ],
+														    color: "#0088cc"
+														}];
+
+														// See: js/examples/examples.dashboard.js for more settings.
+
+													</script>
+
+													<!-- Flot: Sales Porto Drupal -->
+													<div class="chart chart-sm" data-sales-rel="Porto Drupal" id="flotDashSales2" class="chart-hidden"></div>
+													<script>
+
+														var flotDashSales2Data = [{
+														    data: [
+														        ["Jan", 240],
+														        ["Feb", 240],
+														        ["Mar", 290],
+														        ["Apr", 540],
+														        ["May", 480],
+														        ["Jun", 220],
+														        ["Jul", 170],
+														        ["Aug", 190]
+														    ],
+														    color: "#2baab1"
+														}];
+
+														// See: js/examples/examples.dashboard.js for more settings.
+
+													</script>
+
+													<!-- Flot: Sales Porto Wordpress -->
+													<div class="chart chart-sm" data-sales-rel="Porto Wordpress" id="flotDashSales3" class="chart-hidden"></div>
+													<script>
+
+														var flotDashSales3Data = [{
+														    data: [
+														        ["Jan", 840],
+														        ["Feb", 740],
+														        ["Mar", 690],
+														        ["Apr", 940],
+														        ["May", 1180],
+														        ["Jun", 820],
+														        ["Jul", 570],
+														        ["Aug", 780]
+														    ],
+														    color: "#734ba9"
+														}];
+
+														// See: js/examples/examples.dashboard.js for more settings.
+
+													</script>
+												</div>
+
+											</div>
+										</div>
+										<div class="col-xl-4 text-center">
+											<h2 class="card-title mt-3">Sales Goal</h2>
+											<div class="liquid-meter-wrapper liquid-meter-sm mt-3">
+												<div class="liquid-meter">
+													<meter min="0" max="100" value="35" id="meterSales"></meter>
+												</div>
+												<div class="liquid-meter-selector mt-4 pt-1" id="meterSalesSel">
+													<a href="#" data-val="35" class="active">Monthly Goal</a>
+													<a href="#" data-val="28">Annual Goal</a>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</section>
+						</div>
+					</div>
+					<!-- end: page -->
+				</section>
+			</div>
+
+			<aside id="sidebar-right" class="sidebar-right">
+				<div class="nano">
+					<div class="nano-content">
+						<a href="#" class="mobile-close d-md-none">
+							Collapse <i class="fas fa-chevron-right"></i>
+						</a>
+
+						<div class="sidebar-right-wrapper">
+
+							<div class="sidebar-widget widget-calendar">
+								<h6>Upcoming Tasks</h6>
+								<div data-plugin-datepicker data-plugin-skin="dark"></div>
+
+								<!-- ul>
+									<li>
+										<time datetime="2021-04-19T00:00+00:00">04/19/2021</time>
+										<span>Company Meeting</span>
+									</li>
+								</ul-->
+							</div>
+						</div>
+					</div>
+				</div>
+			</aside>
+
+		</section>
+
+		<!-- Vendor -->
+		<script src="vendor/jquery/jquery.js"></script>
+		<script src="vendor/jquery-browser-mobile/jquery.browser.mobile.js"></script>
+		<script src="vendor/popper/umd/popper.min.js"></script>
+		<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+		<script src="vendor/bootstrap-datepicker/js/bootstrap-datepicker.js"></script>
+		<script src="vendor/common/common.js"></script>
+		<script src="vendor/nanoscroller/nanoscroller.js"></script>
+		<script src="vendor/magnific-popup/jquery.magnific-popup.js"></script>
+		<script src="vendor/jquery-placeholder/jquery.placeholder.js"></script>
+
+		<!-- Specific Page Vendor -->
+		<script src="vendor/jquery-ui/jquery-ui.js"></script>
+		<script src="vendor/jqueryui-touch-punch/jquery.ui.touch-punch.js"></script>
+		<script src="vendor/jquery-appear/jquery.appear.js"></script>
+		<script src="vendor/bootstrapv5-multiselect/js/bootstrap-multiselect.js"></script>
+		<script src="vendor/jquery.easy-pie-chart/jquery.easypiechart.js"></script>
+		<script src="vendor/flot/jquery.flot.js"></script>
+		<script src="vendor/flot.tooltip/jquery.flot.tooltip.js"></script>
+		<script src="vendor/flot/jquery.flot.pie.js"></script>
+		<script src="vendor/flot/jquery.flot.categories.js"></script>
+		<script src="vendor/flot/jquery.flot.resize.js"></script>
+		<script src="vendor/jquery-sparkline/jquery.sparkline.js"></script>
+		<script src="vendor/raphael/raphael.js"></script>
+		<script src="vendor/morris/morris.js"></script>
+		<script src="vendor/gauge/gauge.js"></script>
+		<script src="vendor/snap.svg/snap.svg.js"></script>
+		<script src="vendor/liquid-meter/liquid.meter.js"></script>
+		<script src="vendor/jqvmap/jquery.vmap.js"></script>
+		<script src="vendor/jqvmap/data/jquery.vmap.sampledata.js"></script>
+		<script src="vendor/jqvmap/maps/jquery.vmap.world.js"></script>
+		<script src="vendor/jqvmap/maps/continents/jquery.vmap.africa.js"></script>
+		<script src="vendor/jqvmap/maps/continents/jquery.vmap.asia.js"></script>
+		<script src="vendor/jqvmap/maps/continents/jquery.vmap.australia.js"></script>
+		<script src="vendor/jqvmap/maps/continents/jquery.vmap.europe.js"></script>
+		<script src="vendor/jqvmap/maps/continents/jquery.vmap.north-america.js"></script>
+		<script src="vendor/jqvmap/maps/continents/jquery.vmap.south-america.js"></script>
+
+		<!-- Theme Base, Components and Settings -->
+		<script src="js/theme.js"></script>
+
+		<!-- Theme Custom -->
+		<script src="js/custom.js"></script>
+
+		<!-- Theme Initialization Files -->
+		<script src="js/theme.init.js"></script>
+
+		<!-- Examples -->
+		<script src="js/examples/examples.dashboard.js"></script>
+
+	</body>
+</html>
